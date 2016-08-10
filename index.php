6e2e5db6a9ae4c446ea266e7d2dbddff0d74476b
RK <rk_slowan@163.com>
2016-08-10 11:03:50 +0800

    RK
    注释FTP功能

--- index.php
+++ index.php
@@ -3,16 +3,16 @@ set_time_limit(0);
 header("Content-Type:text/html;charset=utf-8");
 date_default_timezone_set('PRC');
 require './config.php';
-use lib\FTP;
-$host = '127.0.0.1';
-$user = 'test';
-$pwd = 'test';      
+// use lib\FTP;
+// $host = '127.0.0.1';
+// $user = 'test';
+// $pwd = 'test';      
 
-$chdir = '/step1';
-$files = 'D:/wamp/www/IPackage/step2/step3/2.txt';
-$obj = new FTP();
-$obj->loginFTP($host, $user, $pwd);
-$obj->upload2FTP($chdir,$files);
+// $chdir = '/step1';
+// $files = 'D:/wamp/www/IPackage/step2/step3/2.txt';
+// $obj = new FTP();
+// $obj->loginFTP($host, $user, $pwd);
+// $obj->upload2FTP($chdir,$files);
 if (isset($_GET['ftp']) && ! empty($_GET['ftp'])) {
     $src = $_GET['ftp'];
     require './view/ftp.htm';
